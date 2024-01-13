54bbd44636fca4541e4433bce42c150fca0efa4c
umair-rao <umairmuqeem@gmail.com>
2024-01-13 16:26:12 +0500

    Fix linters Errors

--- db/migrate/20240113105200_create_likes.rb
+++ db/migrate/20240113105200_create_likes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
       t.references :author, null: false, foreign_key: { to_table: :users }
       t.references :post, null: false, foreign_key: true
-      
+
       t.timestamps
     end
   end
