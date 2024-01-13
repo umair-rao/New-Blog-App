38745bb1af61544b69b959211b42cc1587d61e01
umair-rao <umairmuqeem@gmail.com>
2024-01-14 00:29:40 +0500

    Fix comment test

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Comment, type: :model do
     it 'is valid with a text and author_id' do
       user = User.create(name: 'John', posts_counter: 0)
       post = Post.create(title: 'First post', author_id: user.id, comments_counter: 0, likes_counter: 0)
-      comment = Comment.new(text: 'Great post!', author_id: user.id, post_id: post.id)
+      # comment = Comment.new(text: 'Great post!', author_id: user.id, post_id: post.id)
       expect(post.comments_counter).to eq(0)
     end
   end
