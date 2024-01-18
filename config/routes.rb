ff64ea02aab664a4238a2430826c658dd7f766d4
umair-rao <umairmuqeem@gmail.com>
2024-01-18 19:56:02 +0500

    linters error removed

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :posts do
       resources :comments
-      resources :likes, only: [:create, :destroy]
+      resources :likes, only: %i[create destroy]
     end
   end
 end
