3edc2bb8c11cfefec6bb61acda8deb39ba14c1f9
umair-rao <umairmuqeem@gmail.com>
2024-01-19 19:56:08 +0500

    Added remaining tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe 'GET /users/:id/posts' do
-    it 'returns a successful response' do
-      get '/users/:id/posts'
-      expect(response).to have_http_status(200)
+RSpec.describe 'Posts', type: :request do
+  describe 'Get /posts' do
+    it 'renders index template with correct placeholder text for posts' do
+      get '/posts'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('List of All Posts')
     end
   end
 
-  describe 'GET /users/:id/posts/:id' do
-    it 'returns a successful response' do
-      get '/users/:id/posts/:id'
-      expect(response).to have_http_status(200)
+  describe 'Get /posts/:id' do
+    it 'renders show templates' do
+      get '/posts/id'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Posts Details')
     end
   end
 end
+
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /users' do
-    it 'returns a successful response' do
+RSpec.describe UsersController, type: :request do
+  describe 'Get /users' do
+    it 'renders index template with correct placeholder text for users' do
       get '/users'
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('List of All Users.')
     end
   end
 
-  describe 'GET /users/:id' do
-    it 'returns a successful response' do
-      get '/users/:id'
-      expect(response).to have_http_status(200)
+  describe 'Get /users/:id' do
+    it 'renders show templates' do
+      get '/users/id'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('User Details')
     end
   end
 end
