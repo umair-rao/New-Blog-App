224ff17f86c4f7904d5706430f6448dbec69cdd0
umair-rao <umairmuqeem@gmail.com>
2024-01-20 18:30:36 +0500

    Added forms and validations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,24 @@
 class CommentsController < ApplicationController
     def new
-        @comment = Comment.new
+      @comment = Comment.new
     end
-
+  
     def create
-    @comment = Comment.new(comment_params)
-    @post = Post.find(params[:post_id])
-    @comment.author_id = current_user.id
-    @comment.post_id = @post.id
-
-    if @comment.save
+      @comment = Comment.new(comment_params)
+      @post = Post.find(params[:post_id])
+      @comment.author_id = current_user.id
+      @comment.post_id = @post.id
+  
+      if @comment.save
         redirect_to user_post_path(user_id: @post.author_id, id: @post.id)
-    else
+      else
         render :new, alert: 'Oops! An error has been occurred while creating the comment.'
+      end
     end
-    end
-
+  
     private
-
+  
     def comment_params
-    params.require(:comment).permit(:text)
+      params.require(:comment).permit(:text)
     end
-end
+  end  
